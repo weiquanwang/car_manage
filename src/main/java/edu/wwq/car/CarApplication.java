679aad06edbbc6db0cb380e894d97c5fe70f0fcc
wangweiquan <250382195@qq.com>
2018-05-18 21:05:36 +0800

    Initial commit

---
+++ src/main/java/edu/wwq/car/CarApplication.java
@@ -0,0 +1,12 @@
+package edu.wwq.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarApplication.class, args);
+    }
+}
